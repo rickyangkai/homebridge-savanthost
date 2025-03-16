e9893cffe3721630e3bd7bc1818c533d0013c008
rickyangkai <723307550@qq.com>
2025-03-16 17:16:17 +0800

    fix: 优化 ESLint 配置，修复所有 linter 错误

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,59 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
+export default [
+  eslint.configs.recommended,
   {
-    ignores: ['dist/**'],
-  },
-  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        NodeJS: 'readonly',
+        Buffer: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
     rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'ignoreRestSiblings': true,
+        'args': 'none',
+      }],
+      '@typescript-eslint/no-use-before-define': ['error', { 'functions': false, 'classes': false }],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'no-unused-vars': 'off',
       'quotes': ['error', 'single'],
-      'indent': ['error', 2, { 'SwitchCase': 0 }],
+      'indent': ['error', 2, { 'SwitchCase': 1 }],
       'linebreak-style': ['error', 'unix'],
       'semi': ['error', 'always'],
       'comma-dangle': ['error', 'always-multiline'],
       'dot-notation': 'error',
       'eqeqeq': ['error', 'smart'],
       'curly': ['error', 'all'],
-      'brace-style': ['error'],
+      'brace-style': ['error', '1tbs'],
       'prefer-arrow-callback': 'warn',
-      'max-len': ['warn', 160],
+      'max-len': ['warn', { 'code': 160 }],
       'object-curly-spacing': ['error', 'always'],
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': ['error', { 'classes': false, 'enums': false }],
-      '@typescript-eslint/no-unused-vars': ['error', { 'caughtErrors': 'none' }],
     },
   },
   {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-    },
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+    ],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+];
