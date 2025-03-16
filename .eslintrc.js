f50eeb8ab475d3282548e098f6727ae889b40fe7
rickyangkai <723307550@qq.com>
2025-03-16 18:30:56 +0800

    fix: 更新 ESLint 配置为传统格式

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  globals: {
+    NodeJS: 'readonly',
+    Buffer: 'readonly',
+    setTimeout: 'readonly',
+    clearTimeout: 'readonly',
+    setInterval: 'readonly',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_',
+      'ignoreRestSiblings': true,
+      'args': 'none',
+    }],
+    '@typescript-eslint/no-use-before-define': ['error', { 'functions': false, 'classes': false }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-unused-vars': 'off',
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'dot-notation': 'error',
+    'eqeqeq': ['error', 'smart'],
+    'curly': ['error', 'all'],
+    'brace-style': ['error', '1tbs'],
+    'prefer-arrow-callback': 'warn',
+    'max-len': ['warn', { 'code': 160 }],
+    'object-curly-spacing': ['error', 'always'],
+  },
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+  ],
+}; 
